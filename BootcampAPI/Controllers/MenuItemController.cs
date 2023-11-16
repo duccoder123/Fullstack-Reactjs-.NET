573e659fb5745a47e5b61457444ce1ccc83a6d43
Teo <8duchuynh@gmail.com>
2023-11-16 22:00:41 +0700

    Add project files.

---
+++ BootcampAPI/Controllers/MenuItemController.cs
@@ -0,0 +1,151 @@
+﻿using BootcampAPI.Data;
+using BootcampAPI.Models;
+using BootcampAPI.Models.Dto;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace BootcampAPI.Controllers
+{
+    [Route("api/MenuItem")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        private readonly ApplicationDbContext _db;
+        private ApiResponse _response;
+        public MenuItemController(ApplicationDbContext db)
+        {
+            _db = db;
+            _response = new ApiResponse();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetMenuItems()
+        {
+            _response.Result = _db.MenuItems;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_db.MenuItems.ToList());
+        }
+
+        [HttpGet("{id:int}", Name = "GetMenuItem")]
+        public async Task<IActionResult> GetMenuItem(int id)
+        {
+            if (id == 0)
+            {
+                _response.StatusCode = HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+            MenuItem menuItem = _db.MenuItems.FirstOrDefault(p => p.Id == id);
+            if (menuItem == null)
+            {
+                _response.StatusCode = HttpStatusCode.NotFound;
+                _response.IsSuccess=false;
+                return NotFound(_response);
+            }
+            _response.Result = menuItem;
+            _response.StatusCode = HttpStatusCode.OK;
+            return Ok(_response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateMenuItem([FromBody] MenuItemCreateDto menuItemCreateDto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    MenuItem menuItemCreate = new()
+                    {
+                        Name = menuItemCreateDto.Name,
+                        Price = menuItemCreateDto.Price,
+                        Category = menuItemCreateDto.Category,
+                        SpecialTag = menuItemCreateDto.SpecialTag,
+                        Description = menuItemCreateDto.Description,
+                        Image = menuItemCreateDto.Image,
+                    };
+                    _db.MenuItems.Add(menuItemCreate);
+                    _db.SaveChanges();
+                    _response.Result = menuItemCreate;
+                    _response.StatusCode = HttpStatusCode.Created;
+                    return CreatedAtRoute("GetMenuItem", new { id = menuItemCreate.Id }, _response);
+                }
+                else
+                {
+                    _response.IsSuccess = false;
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return Ok(_response);
+        }
+        [HttpPut("{id:int}")]
+        public async Task<IActionResult> UpdateMenuItem(int id, [FromBody] MenuItemUpdateDto menuItemUpdateDto)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    MenuItem menuItem = _db.MenuItems.FirstOrDefault(x => x.Id == id);
+                    if (menuItem == null)
+                    {
+                        _response.StatusCode = HttpStatusCode.NotFound;
+                        _response.IsSuccess = false;
+                        return BadRequest();
+                    }
+                    menuItem.Name = menuItemUpdateDto.Name;
+                    menuItem.Description = menuItemUpdateDto.Description;
+                    menuItem.Image = menuItemUpdateDto.Image;
+                    menuItem.Price = menuItemUpdateDto.Price;
+                    menuItem.Category = menuItemUpdateDto.Category;
+                    menuItem.SpecialTag = menuItemUpdateDto.SpecialTag;
+
+                    _db.MenuItems.Update(menuItem);
+                    _db.SaveChanges();
+                    _response.Result = menuItemUpdateDto;
+                    _response.StatusCode = HttpStatusCode.NoContent;
+                    return Ok(_response);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string> { ex.ToString() };
+            }
+            return Ok(_response);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteMenuItem(int id)
+        {
+            try
+            {
+                if(id == 0)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.IsSuccess = false;
+                    return BadRequest();
+                }
+                MenuItem item = _db.MenuItems.FirstOrDefault(x => x.Id == id);
+                if (item == null)
+                {
+                    _response.StatusCode = HttpStatusCode.NotFound;
+                    _response.IsSuccess = false;
+                    return BadRequest();
+                }
+                _db.MenuItems.Remove(item);
+                _db.SaveChanges();
+                _response.StatusCode = HttpStatusCode.NoContent;
+                return Ok(_response);
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages = new List<string>() { ex.ToString() };
+            }
+            return Ok(_response);
+        }
+    }
+}
---
+++ BootcampAPI/Program.cs
@@ -0,0 +1,42 @@
+using BootcampAPI.Data;
+using BootcampAPI.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+                builder =>
+                {
+                    builder
+                    .WithOrigins("http://localhost:3000")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials();
+                }));
+// Add services to the container.
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+builder.Services.AddIdentity<ApplicationUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseCors(c => c.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
