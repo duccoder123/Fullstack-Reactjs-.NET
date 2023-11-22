32a1a0864749c501255a14d2e7c470f81093a309
Teo <8duchuynh@gmail.com>
2023-11-22 22:29:39 +0700

    add stripe client to action

--- BootcampAPI/Controllers/PaymentController.cs
+++ BootcampAPI/Controllers/PaymentController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BootcampAPI.Data;
+using BootcampAPI.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Stripe;
 
 namespace BootcampAPI.Controllers
 {
@@ -7,5 +11,46 @@ namespace BootcampAPI.Controllers
     [ApiController]
     public class PaymentController : ControllerBase
     {
+        protected ApiResponse _response;
+        private readonly IConfiguration _configuration;
+        private readonly ApplicationDbContext _db;
+        public PaymentController(ApplicationDbContext db, IConfiguration configuration)
+        {
+            _db = db;
+            _configuration = configuration;
+            _response = new();
+        }
+        [HttpPost]
+        public async Task<ActionResult<ApiResponse>> MakePayment(string userId)
+        {
+            ShoppingCart shoppingCart = _db.ShoppingCarts
+                .Include(u => u.CartItems)
+                .ThenInclude(u => u.MenuItem)
+                .FirstOrDefault(u => u.UserId == userId);
+            if (shoppingCart == null || shoppingCart.CartItems == null || shoppingCart.CartItems.Count() == 0)
+            {
+                _response.StatusCode = System.Net.HttpStatusCode.BadRequest;
+                _response.IsSuccess = false;
+                return BadRequest(_response);
+            }
+
+            #region Create Payment Intent
+            StripeConfiguration.ApiKey = _configuration["StripeSettings:SecretKey"];
+            shoppingCart.CartTotal = shoppingCart.CartItems.Sum(u => u.Quantity * u.MenuItem.Price);
+            PaymentIntentCreateOptions options = new()
+            {
+                Amount = (int)(shoppingCart.CartTotal * 100),
+                Currency = "usd",
+                PaymentMethodTypes = new List<string> { "card" },
+            };
+            PaymentIntentService service = new();
+            PaymentIntent response = service.Create(options);
+            shoppingCart.StripePaymentIntentId = response.Id;
+            shoppingCart.ClientSecret = response.ClientSecret;
+            #endregion
+            _response.Result = shoppingCart;
+            _response.StatusCode = System.Net.HttpStatusCode.OK;
+            return Ok(_response);
+        }
     }
 }
